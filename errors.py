7b08daa97d6b211460a6bbdb6ec273ca0d4c6e94
Piotr Bauer <65217796+Bauero@users.noreply.github.com>
2023-04-02 22:44:43 +0200

    Stack Ready to use (#1)
    * Przygotowanie do oop|
    
    * Dodanie podstawowego mova z rejestrami
    
    * initial commit
    
    * ready to work
    
    * Dodanie obslogi mov
    
    * Dodanie sub, i poprawa dod bit
    
    * Dodanie flagi overflow
    
    * Dodanie inc & dec
    
    * writeIntoRegister and propToBinConv method added
    
    * Cleanup, and added comments
    
    * Added register in print + BI -> SP
    
    * Prep to allow var. & addresses
    
    * Cleanup & error rem from numberToList
    
    * Preparation for addresing
    
    * first address addition working
    
    * backoup
    
    * Preparation for single argument
    
    * Begin implementing bigger values
    
    * stage all fils before another branch
    
    * preparing to work on stack
    
    * Read/Write to Stack
    
    * fixing register sizes
    
    * correction of I/O stack

---
+++ errors.py
@@ -0,0 +1,29 @@
+###     REGISTER ERRORS
+
+class RegisterNotImplemented (Exception): pass
+
+class RegisterTooSmallToMove (Exception): pass
+
+class RegisterSizeTooSmall (Exception): pass
+
+class OperationNotPossible (Exception): pass
+
+class RegisterCantEffectiveAddress (Exception): pass
+
+class RegisterNotWritable (Exception): pass
+
+###     NUMBER ERROR
+
+class WrongNumberBase (Exception):  pass
+
+class NumberTooBig (Exception): pass
+
+###     OTHER ERRORS
+
+class OperandSizeNotSpecified (Exception): pass
+
+class EffectiveAddressError (Exception): pass
+
+class EffectiveAddresNotExist (Exception): pass
+
+class VariableAddressNotExisting (Exception): pass
---
+++ todo.py
@@ -0,0 +1,78 @@
+def ToDo():
+	#?	- to implement
+	def CMP(a1, a2): pass
+	
+	#?	- to implement
+	def MUL(s): pass
+	
+	#?	- to implement
+	def DIV(s): pass
+
+	#?	- to implement
+	def RET(): pass
+
+	#?	- to implement
+	def CALL(d): pass
+
+	#?	- to implement
+	def LOOP(d): pass
+
+	#?	- to implement
+	def NEG(r): pass
+
+	#?	- to implement
+	def AND(r, s): pass
+
+	#?	- to implement
+	def INT(i): pass
+
+	#?	- to implement
+	def PUSH(v): pass
+
+	#?	- to implement
+	def POP(r, s): pass
+	
+
+
+	
+	#?	- to implement
+	def JMP(d): pass
+
+	#?	- to implement
+	def JE(d): pass
+
+	#?	- to implement
+	def JNE(d): pass
+
+	#?	- to implement
+	def JA(d): pass
+
+	#?	- to implement
+	def JNA(d): pass
+
+	#?	- to implement
+	def JB(d): pass
+
+	#?	- to implement
+	def JNB(d): pass
+
+	#?	- to implement
+	def JL(d): pass
+
+	#?	- to implement
+	def JNL(d): pass
+
+	#?	- to implement
+	def JG(d): pass
+
+	#?	- to implement
+	def JNG(d): pass
+
+	#?	- to implement
+	def JZ(d): pass
+
+	#?	- to implement
+	def JC(d): pass
+
+	#?	- to implement
+	def JS(d): pass
