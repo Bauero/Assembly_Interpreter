9925bdedbf751ee5a541021f662709ffda768a21
Piotr Bauer <piotr.bauer.konto@gmail.com>
2023-05-05 10:10:14 +0200

    main readme cleanup

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-"""
-############## PL ##############
-
-To jest program który ma za zadanie pomagać w pracy z Assemblerem 16 bit
-Celem tego programu będzie zrobienie aplikacji która w okienku wyświetla
-Stan naszych rejestrów a następnie dokonuje wpisanych przez nas operacji
-Dlatego zadaniem tego kodu będzie wizualizacja zmian poprzez wypisywanie 
-tego co dzieje się z naszym kodem
-
-############## EN ##############
-
-This is program which purpose is to help learn 16 bit assembly
-The goal is to make an GUI app which, in window, shows status of 
-all registers, and then executs the given instructions
-Therefore the purpose of this code is to visualize changes made by user
-As a possible extenstion, mayby execution of a file may be added later
-so the code should allow it
-
-"""
-
-#	Oznaczenia
-#	"#?"    - something need to be added later !!!
-
 #########################	IMPORT NEEDED MODULES	  #########################
 
 
