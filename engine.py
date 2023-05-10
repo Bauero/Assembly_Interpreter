8bfa94f1736302be7516c5ad4feb19e13a2aef67
Piotr Bauer <piotr.bauer.konto@gmail.com>
2023-05-10 13:52:39 +0200

    function signature

--- engine.py
+++ engine.py
@@ -7,9 +7,30 @@ from datatypes import Variable
 from multipurpose_registers import printRegisters
 from flag_register import printFlagsSpec
 from stack import saveValueToStack, printStack
+from inspect import signature
 
 # linkgage functions with it's names
-funkcje = {k:v for k,v in globals().items() if k.isupper()}
+defFunctions = {k:v for k,v in globals().items() if k.isupper()}
+funcArguments = {}
+
+# defining the function signature and default argument types needed
+for f in defFunctions:
+    if type(defFunctions[f]) != dict:
+        sgt = str(signature(defFunctions[f]))[1:-1].split(", ")
+        for element in range(len(sgt)):
+            if "=" in sgt[element]:
+                end = sgt[element].index('')
+                sgt[element] = sgt[element][:end+1]
+
+        funcArguments[f] = dict()
+        for g in sgt:
+            if g == '':
+                continue
+            sig = str(signature(defFunctions[f]).parameters[g].annotation.__name__)
+            funcArguments[f][g] = sig
+    else:
+        funcArguments[f] = "dict"
+
 
 # analyzes the request
 # if the function exists
