d3ae0224c84f2ceadb94da7f9d255ed32b39146a
Piotr Bauer <piotr.bauer.konto@gmail.com>
2023-07-03 22:11:48 +0200

    lineDecompostion
    Now, line can be decomposed, into individual peaces - so from "add ax,        10" one would obtain ["add", "ax", "10"]

--- decomposer.py
+++ decomposer.py
@@ -1,10 +1,15 @@
 """
 The contents of this file is responsible for translating the command
-from human readible line into peaces whic whould be able to execute 
+from human readible line into peaces which whould be able to execute 
 by the program
 """
 
-def preprocessing(line : str) -> any | None:
+import re
+
+def lineCleanup(line : str) -> str | None:
+    """
+    chceck if string not empty and not comment -> lets it through
+    """
 
     # Empty line
     if len(line) == 0: return None
@@ -25,3 +30,51 @@ def preprocessing(line : str) -> any | None:
                 break
         
         return nline
+
+def lineDecomposition(line : str) -> list:
+    """
+    Decomposition of line into order and arguments
+    """
+    
+    # No possibility of line with sentence
+    if not ("'" in line or '"' in line):
+        
+        splitted = []
+        if "," in line:
+            splitted = line.split(",")
+
+        # Cleaning up white cha
+        for e in range(len(splitted)):
+            splitted[e] = splitted[e].strip()
+
+        parts = []
+        for elem in splitted:
+            parts += elem.split(" ")
+
+        for p in range(len(parts)-1,-1,-1):
+            if parts[p] == "":
+                parts.pop(p)
+
+        return parts
+    
+    # There are ' or " in this line which suggest string
+    else:
+        (lap, rap, lq, rq) = (len(line), len(line), 0, 0)
+        if "'" in line:
+            lap = line.index("'")
+            rap = line[-1::-1].index("'")
+        if '"' in line:
+            lq = line.index('"')
+            rq = line[-1::-1].index('"')
+
+        ul = min(lq, lap) # ultimate left start of string-like thing
+        ur = max(rq, rap) # ultimate right end of string-like thing
+
+        parts = line[:ul].split(" ") + [line[ul:ur+1]] + \
+            line[ur:].split(" ")
+
+        for p in range(len(parts)-1,-1,-1):
+            if parts[p] == "":
+                parts.pop(p)
+
+        return parts
