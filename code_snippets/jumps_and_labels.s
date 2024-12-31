e3c27976e8f5c813f7fed25c5052b2a7f9a9bcad
Piotr Bauer <piotr.bauer.konto@gmail.com>
2024-12-31 01:20:44 +0100

    Add code snippet to test jump instructions

---
+++ code_snippets/jumps_and_labels.s
@@ -0,0 +1,22 @@
+XOR AX,AX
+XOR BX,BX
+XOR CX,CX
+XOR DX,DX
+
+;CLC
+jb  jeden
+jnb drugi
+jmp koniec
+
+jeden:
+    ADD AX, 1
+    jmp koniec
+
+drugi:
+    ADD AX, 2
+    jmp koniec
+
+koniec:
+    pushf
+    pop cx
+    call wyswRej
