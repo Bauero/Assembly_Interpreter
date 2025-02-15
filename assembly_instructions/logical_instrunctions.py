f8077e599025621d0c44308f39295ee9cbc9294a
Piotr Bauer <piotr.bauer.konto@gmail.com>
2025-02-15 17:53:13 +0100

    Add TEST instruction and simplify attributes assignement in logical_instructions

--- assembly_instructions/logical_instrunctions.py
+++ assembly_instructions/logical_instrunctions.py
@@ -19,32 +19,14 @@ def AND(HardwareRegister : HardwareRegisters,
     """Performs logical and operation on two numbers"""
 
     final_size = kwargs['final_size']
+    values_in_int = kwargs["args_values_int"]
 
-    # Convert both numbers to be in raw binary form -> 0111010110101101
-    values_in_binary = []
-    
-    for v in kwargs['args_values_raw']:
-        output = convert_number_to_bit_list(v, final_size)
-        values_in_binary.append(output)
-        
-    # Perform binary and operation
-    output = []
-    auxiliary_carry = 0
-    for bit in range(-1, - final_size -1, -1):
-        b1 = int(values_in_binary[0][bit])
-        b2 = int(values_in_binary[1][bit])
-        result = b1 * b2
-        carry = result == 1
-        output.insert(0, str(result))
-        if abs(bit) == 4:
-            auxiliary_carry = carry
-
-    # Resuce size of number if needed
+    and_value = values_in_int[0] ^ values_in_int[1]
+    output = convert_number_to_bit_list(and_value, final_size).zfill(final_size)
     output = output[-final_size:]   # {final_size} bits from the end
 
     previous_flags = list(FlagRegister.readFlags())
 
-    # Set appriopriate flags
     FlagRegister.setFlag("ZF", not "1" in output)   # if any "1", ZF if OFF
     FlagRegister.setFlag("SF", output[0] == "1")
     FlagRegister.setFlag("CF", 0)
@@ -220,23 +202,50 @@ def NOP(HardwareRegister : HardwareRegisters,
 
     return {}
 
-AND.params_range = [2]
-AND.allowed_params_combinations = [
-    ("memory", "value"), ("memory", "register"), ("register", "register"), 
-    ("register", "value"), ("register", "memory")
-]
+def TEST(HardwareRegister : HardwareRegisters, 
+        FlagRegister : FlagRegister,
+        Data : Data,
+        Variables : dict,
+        Labels : dict,
+        **kwargs):
+    """Logical compare between elements
+    
+    Argumen1 ^ Argument2
+    """
+    final_size = kwargs['final_size']
 
-OR.params_range = [2]
-OR.allowed_params_combinations = [
-    ("memory", "value"), ("memory", "register"), ("register", "register"), 
-    ("register", "value"), ("register", "memory")
-]
+    values_in_int = kwargs["args_values_int"]
 
-XOR.params_range = [2]
-XOR.allowed_params_combinations = [
-    ("memory", "value"), ("memory", "register"), ("register", "register"), 
-    ("register", "value"), ("register", "memory")
-]
+    and_value = values_in_int[0] ^ values_in_int[1]
+    output = convert_number_to_bit_list(and_value, final_size).zfill(final_size)
+    output = output[-final_size:]
+
+    previous_flags = list(FlagRegister.readFlags())
+
+    FlagRegister.setFlag("ZF", not "1" in output)
+    FlagRegister.setFlag("SF", output[0] == "1")
+    FlagRegister.setFlag("CF", 0)
+    FlagRegister.setFlag("PF", eval_no_of_1(output))
+    FlagRegister.setFlag("OF", 0)
+    
+    new_flags = list(FlagRegister.readFlags())
+    
+    all_changes = {
+        "flags" : {
+            "oryginal_value" :  previous_flags,
+            "new_value" :       new_flags
+        }
+    }
+
+    return all_changes
+
+for fn in [AND, OR, XOR, TEST]:
+    """Assign all functions the same attributes"""
+    fn.params_range = [2]
+    fn.allowed_params_combinations = [
+        ("memory", "value"), ("memory", "register"), ("register", "register"), 
+        ("register", "value"), ("register", "memory")
+    ]
 
 NOT.params_range = [1]
 NOT.allowed_params_combinations = [ ("memory",), ("register",) ]
