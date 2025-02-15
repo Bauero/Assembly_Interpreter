b1052b6ffc020f3533619cfda15ab94e6bdb6c14
Piotr Bauer <piotr.bauer.konto@gmail.com>
2025-02-15 18:56:07 +0100

    Add SHR instruction

--- assembly_instructions/bit_movement_instructions.py
+++ assembly_instructions/bit_movement_instructions.py
@@ -59,6 +59,56 @@ def SHL(HardwareRegister : HardwareRegisters,
 
     return all_changes
 
+def SHR(HardwareRegister : HardwareRegisters, 
+        FlagRegister : FlagRegister,
+        Data : Data,
+        Variables : dict,
+        Labels : dict,
+        **kwargs):
+    """SHIFT LOGICAL RIGHT"""
+
+    final_size = kwargs['final_size']
+
+    value_to_shift = convert_number_to_bit_list(kwargs['args_values_raw'][0], final_size)
+    rotation_counter = kwargs['args_values_int'][1] % final_size
+
+    first_bit = value_to_shift[0] == "1"
+    carry = "0"
+    sign = first_bit
+    
+    for shift in range(rotation_counter):
+        value_to_shift.insert(0, "0")
+        carry = value_to_shift[-1]
+        sign = value_to_shift[0]
+        value_to_shift = value_to_shift[:-1]
+
+    first_bit_after = value_to_shift[0] == "1"
+
+    backup_flags = FlagRegister.readFlags()
+
+    FlagRegister.setFlag("OF", first_bit != first_bit_after)
+    FlagRegister.setFlag("SF", sign == "1")
+    FlagRegister.setFlag("CF", carry == "1")
+    FlagRegister.setFlag("ZF", not "1" in value_to_shift)
+    FlagRegister.setFlag("PF", eval_no_of_1(value_to_shift))
+
+    new_flags = FlagRegister.readFlags()
+
+    m = save_value_in_destination(HardwareRegister, Data, Variables, value_to_shift,
+                             kwargs['param_types'][0], kwargs['source_params'][0])
+
+    all_changes = {
+        m[0] : [
+            m[1]
+        ],
+        "flags" : {
+            "oryginal_value" :  backup_flags,
+            "new_value" :       new_flags
+        }
+    }
+
+    return all_changes
+
 def SAL(HardwareRegister : HardwareRegisters, 
         FlagRegister : FlagRegister,
         Data : Data,
